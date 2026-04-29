5a69a71265ebccf1dacf43041b926d2cfb4bd497
saqib68 <saqibghouri001@gmail.com>
2026-04-29 18:04:31 +0500

    Resturant Login and Roles

--- Backend/routes/restaurants.js
+++ Backend/routes/restaurants.js
@@ -4,6 +4,8 @@ const {
   getTrendingRestaurants,
   getRestaurantById,
   createRestaurant,
+  getMyRestaurants,
+  deleteRestaurant,
 } = require('../controllers/restaurantController');
 const { auth } = require('../middleware/auth');
 
@@ -11,7 +13,9 @@ const router = express.Router();
 
 router.get('/', getRestaurants);
 router.get('/trending', getTrendingRestaurants);
+router.get('/my', auth, getMyRestaurants);
 router.get('/:id', getRestaurantById);
 router.post('/', auth, createRestaurant);
+router.delete('/:id', auth, deleteRestaurant);
 
 module.exports = router;
